153664db78c773eadc1a2d6ec5809fa39f17cce7
kierandenyer <kieranjd13@outlook.com>
2022-03-31 11:38:42 -0400

    code

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Welcome To The Maze\n")
+def scenarios():
+
+def direction():
+    if dir == "left":
+        return "c1"
+    if dir == "right"
+        return "c2"
+
+d1 = input("You come across a hedge maze in the forest, you decide to try your luck and enter. \n\n is yoyr fisrt turn left or right?\n")
+d1 = dir
